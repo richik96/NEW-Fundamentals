b935664821a79595561118e100a207b8b84510d2
richik96 <drichik@gmail.com>
2024-09-25 12:59:41 +0530

    DSA practice

---
+++ DSA/BinarySearch.java
@@ -0,0 +1,30 @@
+//package DSA;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int key = 5;
+        int result = binarySearch(arr, key);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+
+    static int binarySearch(int arr[], int key) {
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
